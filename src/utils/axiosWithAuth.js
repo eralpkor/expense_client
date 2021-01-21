ebad11aecf4be1fc1d33ed26c073f33631b95c13
eralpkor <eralp.ny@gmail.com>
2021-01-21 14:07:01 -0500

    Add axios with auth

---
+++ src/utils/axiosWithAuth.js
@@ -0,0 +1,19 @@
+import axios from 'axios';
+
+export const axiosWithAuth = () => {
+  const token = localStorage.getItem('token');
+
+  return axios.create({
+    baseURL: 'http://localhost:4000/auth',
+    headers: {
+      Authorization: token,
+    }
+  });
+}
+
+// clear the token with logout
+export const logout = () => {
+  localStorage.clear();
+  window.location.href = window.location.origin + '/';
+}
+
