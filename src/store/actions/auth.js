87f150705b3b6daf457bf625e39d0ad465eb22f6
eralpkor <eralp.ny@gmail.com>
2021-03-05 11:28:02 -0500

    Add clear_message to logout function

--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -5,37 +5,38 @@ import {
   LOGIN_FAIL,
   LOGOUT,
   SET_MESSAGE,
+  CLEAR_MESSAGE,
   JWT_EXPIRED,
   USER_UPDATE_SUCCESS,
   USER_UPDATE_FAILED,
+  USER_EXPENSE_SUCCESS,
+  USER_EXPENSE_FAILED,
 } from './types';
 
 import AuthService from '../../services/auth.service';
 
-export const register = (username, email, password) => (dispatch) => {
+export const registerUser = (username, email, password) => (dispatch) => {
   return AuthService.register(username, email, password).then(
-    (data) => {
+    (response) => {
       dispatch({
         type: REGISTER_SUCCESS,
-        payload: {user: data }
       });
-console.log('THIS IS FROM REGISTER ', data)
+console.log('THIS IS FROM REGISTER ', response)
 
       dispatch({
         type: SET_MESSAGE,
-        payload: data.data.message,
+        payload: response.data.message,
       });
 
       return Promise.resolve();
     },
     (error) => {
-      // let message = '';
       let username = error.response.data.errors[0].username;
       let email = error.response.data.errors[0].email;
       var message = [];
 
       if (error.response.data.errors.length === 0) {
-        console.log('No fucking errors ')
+        console.log('No errors ')
       }
       if (error.response.data.errors.length === 1) {
         if (error.response.data.errors[0].username) {
@@ -101,7 +102,6 @@ export const updateUserInfo = (data) => (dispatch) => {
   return AuthService.updateUserInfo(data)
     .then(data => {
   console.log('data from actions/auth edituser ', data)
-
       dispatch({
         type: USER_UPDATE_SUCCESS,
         payload: {data: data}
@@ -136,4 +136,45 @@ export const logout = () => (dispatch) => {
   dispatch({
     type: LOGOUT,
   });
+  dispatch({
+    type: CLEAR_MESSAGE,
+  })
 };
+
+export const getUserExpenses = () => (dispatch) => {
+  return AuthService.getUserExpense()
+    .then(data => {
+      console.log('USER EXPeNESE FROM AUTH ', data.message);
+
+      if (data.message) {
+        dispatch({
+          type: SET_MESSAGE,
+          payload: data.message,
+        });
+        // return Promise.resolve();
+      }
+      dispatch({
+        type: USER_EXPENSE_SUCCESS,
+        payload: { data: data }
+      })
+      return Promise.resolve();
+    },
+    (errors) => {
+      const message = 
+        (errors.response && 
+          errors.response.data.message && 
+          errors.data.message) ||
+          errors.message ||
+          errors.toString();
+
+      dispatch({
+        type: USER_EXPENSE_FAILED,
+      });
+
+      dispatch({
+        type: SET_MESSAGE,
+        payload: message,
+      })
+      }
+    )
+}
