f2aa13b28c797a28cad403b43199c9cb9c13eb16
eralpkor <eralp.ny@gmail.com>
2021-05-18 13:43:17 -0400

    Add main container component

---
+++ src/components/MainContainer.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import Container from "@material-ui/core/Container";
+import { makeStyles } from "@material-ui/core/styles";
+import CssBaseline from "@material-ui/core/CssBaseline";
+
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    marginTop: theme.spacing(4),
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+  },
+}));
+
+export const MainContainer = ({ children, ...props }) => {
+  const styles = useStyles();
+
+  return (
+
+    <Container
+      className={styles.root}
+      component="main"
+      maxWidth="xs"
+      {...props}
+    >
+    <CssBaseline />
+      
+      {children}
+    </Container>
+  );
+};
