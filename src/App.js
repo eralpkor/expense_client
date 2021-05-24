6bb2c7cbdb671d42bab34780471b48ab37c9703b
eralpkor <eralp.ny@gmail.com>
2021-05-24 18:04:03 -0400

    Fix routing issue with landing/404 page

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { Router, Redirect } from "react-router-dom";
 import "./App.css";
 import { Route, Switch } from "react-router-dom";
@@ -12,11 +12,43 @@ import { clearMessage } from "./store/actions/message";
 import { history } from "./helpers/history";
 import Profile from "./components/Profile";
 import NotFoundPage from "./components/NotFoundPage"
+import LandingPage from "./components/Landing";
+// for dark theme
+import { createMuiTheme, ThemeProvider } from "@material-ui/core/styles";
+import {
+  orange,
+  lightBlue,
+  deepPurple,
+  deepOrange
+} from "@material-ui/core/colors";
 
 
 function App() {
   const { isLoggedIn } = useSelector((state) => state.auth);
   const dispatch = useDispatch();
+  const [darkState, setDarkState] = useState(false); // Dark mode
+  const palletType = darkState ? "dark" : "light";
+  const mainPrimaryColor = darkState ? orange[500] : lightBlue[500];
+  const mainSecondaryColor = darkState ? deepOrange[900] : deepPurple[500];
+  // dark theme
+  const darkTheme = createMuiTheme({
+    palette: {
+      type: palletType,
+      typography: { useNextVariants: true },
+
+      primary: {
+        main: mainPrimaryColor
+      },
+      secondary: {
+        main: mainSecondaryColor
+      }
+    }
+  });
+
+  const handleThemeChange = () => {
+    setDarkState(!darkState);
+  }
+  
 
   useEffect(() => {
     history.listen((location) => {
@@ -24,18 +56,19 @@ function App() {
     });
   }, [dispatch]);
 
-  // useEffect(() => {
-  //   if (currentUser) {
-  //     setShowModeratorBoard(currentUser.roles.includes("ROLE_MODERATOR"));
-  //     setShowAdminBoard(currentUser.roles.includes("ROLE_ADMIN"));
-  //   }
-  // }, [currentUser]);
-
   return (
     <Router history={history}>
       <div className="App">
-        <Navigation />
+    <ThemeProvider theme={darkTheme}>
+
         <Switch>
+        <Route exact path='/landing' component={LandingPage} />
+        <div>
+          <Navigation 
+            darkMode={handleThemeChange}
+            checked={darkState}
+            label={darkState ? "Go Light Mode" : "Go Dark Mode"}
+          />
           <Route
             exact
             path="/"
@@ -47,15 +80,28 @@ function App() {
               );
             }}
           />
-          <Route exact path="/login" component={SignIn} />
-          <Route exact path="/register" component={Register} />
-          <PrivateRoute exact path="/home" component={UserHome} />
-          <PrivateRoute exact path="/profile" component={Profile} />
-          <Route component={NotFoundPage} />
+          <Switch>
+            <Route exact path="/login" component={SignIn} />
+            <Route exact path="/register" component={Register} />
+            <PrivateRoute exact path="/home" component={UserHome} />
+            <PrivateRoute exact path="/profile" component={Profile} />
+            <Route component={NotFoundPage} />
+          </Switch>
+          </div>
         </Switch>
+      </ThemeProvider>
+
       </div>
     </Router>
   );
 }
 
 export default App;
+
+
+  // useEffect(() => {
+  //   if (currentUser) {
+  //     setShowModeratorBoard(currentUser.roles.includes("ROLE_MODERATOR"));
+  //     setShowAdminBoard(currentUser.roles.includes("ROLE_ADMIN"));
+  //   }
+  // }, [currentUser]);
