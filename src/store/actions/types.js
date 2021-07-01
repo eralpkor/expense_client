982a6f2b3c4f4d620a45b7d5f834a97abd0076dd
eralpkor <eralp.ny@gmail.com>
2021-07-01 10:32:29 -0400

    Clean code

--- src/store/actions/types.js
+++ src/store/actions/types.js
@@ -1,16 +1,18 @@
+// Register and login
 export const REGISTER_SUCCESS = "REGISTER_SUCCESS";
 export const REGISTER_FAIL = "REGISTER_FAIL";
 export const LOGIN_SUCCESS = "LOGIN_SUCCESS";
 export const LOGIN_FAIL = "LOGIN_FAIL";
 export const LOGOUT = "LOGOUT";
 
+// user updates
 export const JWT_EXPIRED = "JWT_EXPIRED";
 export const USER_UPDATE_SUCCESS = "USER_UPDATE_SUCCESS";
 export const USER_UPDATE_FAILED = "USER_UPDATE_FAILED";
 
 export const SET_MESSAGE = "SET_MESSAGE";
 export const CLEAR_MESSAGE = "CLEAR_MESSAGE";
-
+// get user expenses
 export const USER_EXPENSE_SUCCESS = "USER_EXPENSE_SUCCESS";
 export const USER_EXPENSE_FAILED = "USER_EXPENSE_FAILED";
-// export const USER_EXPENSE_
+// create expense
